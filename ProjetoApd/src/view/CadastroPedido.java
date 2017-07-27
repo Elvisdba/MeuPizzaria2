89025e90f63af4883a9778d1664143733c777f18
Flávio Costa <flavio.danilo022@gmail.com>
2017-07-27 11:23:10 -0300

    Update CadastroPedido.java

--- ProjetoApd/src/view/CadastroPedido.java
+++ ProjetoApd/src/view/CadastroPedido.java
@@ -1,13 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package view;
 
 /**
  *
- * @author 31534112
+ * @author flavio022
  */
 public class CadastroPedido {
     // 1- Para adcicionar Pedido
