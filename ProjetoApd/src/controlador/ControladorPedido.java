69cec39e92715dfdd4a8fffc50337e78f2bfd090
Flávio Costa <flavio.danilo022@gmail.com>
2017-07-27 11:24:16 -0300

    Update ControladorPedido.java

--- ProjetoApd/src/controlador/ControladorPedido.java
+++ ProjetoApd/src/controlador/ControladorPedido.java
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package controlador;
 
 /**
  *
- * @author 31534112
+ * @author flavio022
  */
 public class ControladorPedido {
     
